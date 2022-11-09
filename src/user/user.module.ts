6bdc3d653f19037558c547b4bd967bf5bc3d7a3f
sw1104 <sangw1104@gmail.com>
2022-11-10 01:36:43 +0900

    Feat: 유저 로그인 API 구현

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,18 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: `${process.env.SECRET_KEY}`,
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,23 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserSignUpDto } from './dto/userSignUp.dto';
+import { UserSignInDto } from './dto/userSignIn.dto';
 import * as bcrypt from 'bcrypt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { Payload } from './dto/payload.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwt: JwtService,
+  ) {}
   async userSignUp(userSignUpDto: UserSignUpDto) {
     const { email } = userSignUpDto;
     const duplicate = await this.isExistByEmail(email);
@@ -27,5 +37,20 @@ export class UserService {
 
   async hashedPassword(password: string) {
     return await bcrypt.hash(password, 12);
+  }
+
+  async userSignIn(
+    userSignInDto: UserSignInDto,
+  ): Promise<{ accessToken: string }> {
+    const { email, password } = userSignInDto;
+    const user = await this.isExistByEmail(email);
+    const payload: Payload = { email };
+    const accessToken = await this.jwt.sign(payload);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('이메일 또는 비밀번호를 확인하세요.');
+    }
   }
 }
