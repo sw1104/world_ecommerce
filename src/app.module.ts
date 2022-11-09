1ad258ca7a42d907a315472c684531540e84a69e
sw1104 <sangw1104@gmail.com>
2022-11-09 21:11:25 +0900

    Feat: 유저 회원가입 API 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { MarketModule } from './market/market.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule, MarketModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ProductModule,
+    MarketModule,
+    ConfigModule.forRoot({}),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserSignUpDto } from './dto/userSignUp.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async userSignUp(userSignUpDto: UserSignUpDto) {
+    const { email } = userSignUpDto;
+    const duplicate = await this.isExistByEmail(email);
+    if (duplicate) throw new BadRequestException('중복된 이메일 입니다.');
+    userSignUpDto.password = await this.hashedPassword(userSignUpDto.password);
+    return await this.createUser(userSignUpDto);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async isExistByEmail(email: string) {
+    return await this.userModel.findOne({ email }).lean();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async createUser(userSignUpDto: UserSignUpDto) {
+    const registerUser = new this.userModel(userSignUpDto);
+    registerUser.save();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async hashedPassword(password: string) {
+    return await bcrypt.hash(password, 12);
   }
 }
